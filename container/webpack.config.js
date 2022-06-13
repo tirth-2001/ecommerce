a00a008dd496c8718d715435716d501f4be3fd09
TIRTH PATEL <54476835+tirth-2001@users.noreply.github.com>
2022-06-13 12:13:33 +0530

    prod changes URL

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -10,8 +10,10 @@ module.exports = {
 		new moduleFederationPlugin({
 			name: 'container',
 			remotes: {
-				products: 'products2@http://localhost:8081/remoteEntry.js',
-				cart1: 'cart11@http://localhost:8082/remoteEntry.js',
+				// products: 'products2@http://localhost:8081/remoteEntry.js',
+				products: 'products2@https://microfrontend-ecommerce-products.vercel.app/remoteEntry.js',
+				// cart1: 'cart11@http://localhost:8082/remoteEntry.js',
+				cart1: 'cart11@https://microfrontend-ecommerce-cart.vercel.app/remoteEntry.js',
 			},
 		}),
 		new htmlWebpackPlugin({
